a074dee1c171961cb69232bc22e88cb5e0a43743
travishenson <hello@travishenson.com>
2020-12-12 19:43:16 -0900

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,11 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
+    {
+      resolve: 'gatsby-source-graphcms',
+      options: {
+        endpoint: 'https://api-us-west-2.graphcms.com/v2/ckimksot2ex6d01xy637h5wud/master'
+      }
+    }
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+import { graphql, useStaticQuery } from 'gatsby';
+
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 
 import img_gatsby from 'assets/images/gatsby-astronaut.png';
 
 const IndexPage = () => {
+  const { graphCmsPage = {} } = useStaticQuery(graphql`
+    query PageQuery {
+      graphCmsPage(id: {eq: "Page:ckimkzvjk8m9c0a19qq5lmesd"}) {
+        tagline
+        headline
+        id
+      }
+    }
+  `);
+
+  const { headline, tagline } = graphCmsPage;
+
   return (
     <Layout pageName="home">
       <Helmet>
         <title>Home Page</title>
       </Helmet>
       <Container>
+        <h1>{ headline }</h1>
+        <p>{ tagline }</p>
         <p className="gatsby-astronaut">
           <img src={img_gatsby} alt="Build with Gatsby!" />
         </p>
